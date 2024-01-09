eabd364b6f6cef57265a886bec4798c48c8bf760
John Rogers <jrhawktwo@gmail.com>
2024-01-09 07:28:02 -0600

    added settings

--- western/settings.py
+++ western/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kdxy4sifn#p!2kq7f3pz4w2_=vzqitw&y3d+=s_1$1(4&11vz)"
+SECRET_KEY = os.getenv("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
