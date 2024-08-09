77a7ed039e9063f00a431a65adcee1b270f27ab8
John Rogers <69231460+CrimpsonSlopers@users.noreply.github.com>
2024-08-09 14:03:02 -0500

    Update settings.py

--- western/settings.py
+++ western/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["3.136.163.226", "localhost", "127.0.0.1", "autofeeder.westernfutures.com"]
+ALLOWED_HOSTS = ["3.136.163.226", "localhost", "127.0.0.1", "muddled.live"]
 
 
 # Application definition
