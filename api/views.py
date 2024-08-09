e9440c824a592087ab1cc06b34bc9bfeb086e609
John Rogers <69231460+CrimpsonSlopers@users.noreply.github.com>
2024-08-09 14:45:32 -0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,24 +1,21 @@
 import pandas as pd
-from tqdm import tqdm
-import pytz
+import asyncio
 import json
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated  # Import the IsAuthenticated permission class
 from api.models import *
 from api.AutoFeeder import *
 from api.serializers import *
 from western.settings import *
 
 
-REPORTS_URL = "https://marsapi.ams.usda.gov/services/v1.2/reports"
-
 
 class UserPermissionCheckAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission to the view
 
     def get(self, request):
         user = request.user
@@ -27,9 +24,7 @@ class UserPermissionCheckAPIView(APIView):
             serializer = UserSerializer(user)
             return Response(serializer.data)
         else:
-            return Response(
-                {"detail": "Access Forbidden"}, status=status.HTTP_403_FORBIDDEN
-            )
+            return Response({'detail': 'Access Forbidden'}, status=status.HTTP_403_FORBIDDEN)
 
 
 class AddAuctionFromJSON(APIView):
@@ -71,46 +66,129 @@ class AuctionView(APIView):
                 {"error": "Auction not found"}, status=status.HTTP_404_NOT_FOUND
             )
 
+    def post(self, request):
+        serializer = AuctionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+        else:
+            return Response(
+                {"results": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, id=None):
+        try:
+            auction = Auction.objects.get(id=id)
+            serializer = AuctionSerializer(auction, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+            else:
+                return Response(
+                    {"results": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Auction not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def patch(self, request, id=None):
+        try:
+            auction = Auction.objects.get(id=id)
+            serializer = AuctionSerializer(auction, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+            else:
+                return Response(
+                    {"results": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Auction not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class SaleView(APIView):
+    def get(self, request, id=None):
+        try:
+            if id:
+                sale = Sale.objects.get(id=id)
+                serializer = SaleSerializer(sale)
+                return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+            else:
+                sale = Sale.objects.all()
+                serializer = SaleSerializer(sale, many=True)
+                return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Sale not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def post(self, request):
+        serializer = SaleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+        else:
+            return Response(
+                {"results": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, id=None):
+        try:
+            sale = Sale.objects.get(id=id)
+            serializer = SaleSerializer(sale, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+            else:
+                return Response(
+                    {"results": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Sale not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def patch(self, request, id=None):
+        try:
+            sale = Sale.objects.get(id=id)
+            serializer = SaleSerializer(sale, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+            else:
+                return Response(
+                    {"results": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Sale not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
 
 class UpdateView(APIView):
     def get(self, request, slug=None):
         try:
-            auctions = Auction.objects.all()
-            for auction in auctions:
-                make_request(auction)
+            auction = Auction.objects.get(slug=slug)
+            make_request(auction)
             return Response({"results": "serializer.data"}, status=status.HTTP_200_OK)
 
         except ObjectDoesNotExist:
             return Response(
                 {"error": "Error updating"}, status=status.HTTP_404_NOT_FOUND
             )
-
-
-class ReportView(APIView):
-    def get(self, request, slug=None):
-        try:
-            response = requests.get(REPORTS_URL, auth=(os.getenv("MARS_API_KEY"), ""))
-            reports = {obj["slug_id"]: obj for obj in response.json()}
-            auctions = Auction.objects.all()
-
-            update = []
-            for auction in auctions:
-                report_date = datetime.strptime(
-                    reports[auction.slug]["published_date"], "%m/%d/%Y %H:%M:%S"
-                )
-                localized_datetime = pytz.timezone("UTC").localize(report_date)
-                if localized_datetime > auction.report_date:
-                    update.append(auction)
-
-            for a in tqdm(update):
-                make_request(a)
-
-            return Response(
-                {"data": "serializer.data"},
-                status=status.HTTP_200_OK,
-            )
-
-        except ObjectDoesNotExist:
-            return Response(
-                {"error": "Error updating"}, status=status.HTTP_404_NOT_FOUND
             )
